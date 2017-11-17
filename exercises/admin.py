0c90ceb292942818062db08dc72af6a8257f6346
Yijia Liang <yijial@princeton.edu>
2017-11-17 15:11:24 -0500

    refactoring models

--- exercises/admin.py
+++ exercises/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Exercise
 admin.site.register(Exercise)
+
+from .models import Hint
+admin.site.register(Hint)
--- exercises/models.py
+++ exercises/models.py
@@ -1,16 +1,27 @@
-from django.db import models
-
 # Create your models here.
 from django.db import models
 
 class Exercise(models.Model):
 	question_name = models.CharField(max_length = 200)
 	question_description = models.TextField()
+	question_guidelines = models.TextField() # longer version
 	test_inputs = models.CharField(max_length = 200)
 	test_outputs = models.CharField(max_length = 200)
 	java_equivalent = models.TextField()
 	answer_code = models.TextField()
-	hints = models.TextField() # separate hints by commas so that we can split
+
+	def __str__(self):
+		return self.question_name
+
+class Hint(models.Model):
+	exercise = models.ForeignKey(Exercise, on_delete = models.CASCADE, related_name = 'hints')
+	hint = models.TextField()
+	number = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.exercise.question_name + ': Hint ' + self.number
+
+
 
 
 		
